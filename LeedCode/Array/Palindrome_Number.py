67553980a37eee33c31ee8ec61ee9ada471732cf
anku081 <ankuraj155144@gmail.com>
2026-01-11 10:34:14 +0530

    palindrome number

---
+++ LeedCode/Array/Palindrome_Number.py
@@ -0,0 +1,13 @@
+def isPalindrome(x: int) -> bool:
+    if x<0:
+        return False
+    original_num = x
+    reversed_num = 0
+    while x>0:
+        digit = x%10
+        reversed_num = reversed_num * 10 + digit
+        x = x//10
+    return original_num == reversed_num
+
+x= int(input("Enter a number: "))
+print(isPalindrome(x))
